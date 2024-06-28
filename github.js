b492fbd156f877d5be61cfa719311058c6f6973d
runelahmed <runelahmed71@gmail.com>
2024-06-28 11:21:09 +0300

    simple github-finder app

---
+++ github.js
@@ -0,0 +1,24 @@
+
+
+class GitHub {
+    constructor() {
+        this.client_id = 'c5bf1ff47001964239c7';
+        this.client_secret = '17e99802254bfb4dc0f3f6d2ed10a69fce37ea50';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
